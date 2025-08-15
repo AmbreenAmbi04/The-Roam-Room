101d3c9c12dac992a19d0ffa122749d719ab3b8f
Ambreen <ambreenambi04@gmail.com>
2025-08-15 11:32:08 +0500

    Added Navbar!

--- index.php
+++ index.php
@@ -6,12 +6,45 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     <title>The Roam Room</title>
+    <link rel="icon" href="The Roam Room Logo.jpg" type="image/icon">
 </head>
-<body class="bg-dark text-light">
-  <div>
-    <h1 class="text-center mb-3 mt-3">The Roam Room</h1>
+<body class="bg-black text-light">
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg bg-gradient bg-black navbar-dark fs-5">
+  <div class="container-fluid">
+    <img src="The Roam Room Logo.jpg" alt="Logo" class="navbar-brand" style="width: 70px; height: 70px; border-radius: 250px">
+    <a class="navbar-brand fs-3" href="#">The Roam Room</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Itinerary</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Booking</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Reviews</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
   </div>
-  <!--Accordion-->
+</nav>
+  <!--Itinerary Accordion-->
     <div class="container card pt-3 pb-3 text-white" style="background-color: #430E18">
         <h2 class="pb-2">Itinerary</h2>
     <div class="accordion" id="accordionExample">
@@ -53,26 +86,19 @@
   </div>
 </div>
     </div>
+    <!--Contact Us Form-->
     <div class="card container flex-column mt-5 mb-5">
-        <h2 class="card-header form">Form</h2>
-        <form class="card-body" action="Practice.php" method="POST">
+        <h2 class="card-header form mt-2 pt-2">Contact Us</h2>
+        <form class="card-body" action="index.php" method="POST">
         <label for="name">Name: </label>
         <input type="text" id="name" name="name" class="form-control mb-3">
         <label for="email">Email: </label>
         <input type="email" id="email" name="email" class="form-control mb-3">
-        <label for="phone" id="phone">Phone: </label>
-        <input type="tel" id="phone" name="phone" class="form-control mb-3">
+        <label for="subject" id="subject">Subject: </label>
+        <input type="text" id="subject" name="subject" class="form-control mb-3">
         <button type="submit" class="btn btn-primary mb-5">Submit</button>
         <button type="reset" class="btn btn-secondary mb-5">Reset</button>
-
         </form>
-    </div>
-    
-    <div>
-        <video autoplay controls loop width="600" height="400" class="d-block mx-auto">
-            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
     </div>
 </body>
 </html>
@@ -89,19 +115,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $name= $_POST["name"];
 $email = $_POST["email"];
-$phone = $_POST["phone"];
+$subject = $_POST["subject"];
 
 if ($connection)
 {
     echo "Connected successfully";
-    if (!empty($name) && !empty($email) && !empty($phone))
+    if (!empty($name) && !empty($email) && !empty($subject))
     {
-        $stmt = $connection->prepare("INSERT INTO tbl_practice(name, email, phone) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $name, $email, $phone);
+        $stmt = $connection->prepare("INSERT INTO tbl_practice(name, email, subject) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $name, $email, $subject);
         if ($stmt->execute())
             {
                 echo "<br><br>Data inserted successfully";
-                echo "<br>Name: ". $name . "<br> Email: ". $email . "<br> Phone: " . $phone;
+                echo "<br>Name: ". $name . "<br> Email: ". $email . "<br> Subject: " . $subject;
             };
 
         $select = $connection->prepare("SELECT * FROM tbl_practice");
@@ -112,7 +138,7 @@ if ($connection)
             {
                 while($row = $result->fetch_assoc())
                 {
-                    echo "<br> Name: " . $row["name"] . "<br> Email: " . $row["email"] . "<br> Phone: " . $row["phone"];
+                    echo "<br> Name: " . $row["name"] . "<br> Email: " . $row["email"] . "<br> Subject: " . $row["subject"];
                 }
             }
         }
